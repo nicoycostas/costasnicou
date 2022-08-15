fd5632696809f61af0d3a34621db6f04ef36ee58
Costas Nicou <nicoycostas@gmail.com>
2022-08-15 22:15:15 +0300

    blog-thubmnail-fix

--- front-page.php
+++ front-page.php
@@ -125,12 +125,11 @@
             <article>
               
                 <!-- image -->
-                <div class="img" style="background-size:cover; background-image: url(<?php echo the_post_thumbnail_url();?>)">
+                <div class="img" style="background-size:cover; background-position: center; background-image: url(<?php echo the_post_thumbnail_url();?>)">
                     
                 </div>
                 <h3><?php the_title(); ?></h3>
                 <?php the_excerpt() ?>
-
                 <!-- butn -->
                 <a href="<?php echo get_permalink(); ?>" class="btn-blog">Read More</a>
             </article>

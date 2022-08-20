3ce14f221aa0ea6a6ffeb9d426f052e2bbaac81d
Costas Nicou <nicoycostas@gmail.com>
2022-08-20 10:32:41 +0300

    head_title_fix

--- header.php
+++ header.php
@@ -5,16 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/5a00dc9b71.js" crossorigin="anonymous"></script>
-    <!-- <link rel="stylesheet" href="assets/css/main.css"> -->
-    <!-- <link rel="stylesheet" href="./assets/css/app.css"> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Merienda&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300&display=swap" rel="stylesheet">
-    <script src="./assets/js/script.js" defer></script>
-    <title><?php wp_title(''); ?></title>
+    <?php wp_title(''); ?> 
     <?php wp_head(); ?>
 </head>
 
@@ -28,7 +25,7 @@
         <nav id="menu" class="menu">
             <p class="welcome-msg">Made with Coffee</p>
             <label for="toggle">&#9776;</label>
-            <input type="checkbox" name="" id="toggle">
+            <input type="checkbox" name="checkbox" id="toggle">
         
             <ul id="menu-bar" class="menu-bar">
                 <li class="menu-list-item"><a class="menu-link" href="<?php echo get_home_url(); ?>">Home</a></li>

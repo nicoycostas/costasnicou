9ff5d1950a06d3a0794c2a4b8507e79d9ca489f7
Costas Nicou <nicoycostas@gmail.com>
2022-10-19 22:23:02 +0300

    google-fonts-minimization

--- header.php
+++ header.php
@@ -7,10 +7,7 @@
     <script src="https://kit.fontawesome.com/5a00dc9b71.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Merienda&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Merienda:wght@400;700&family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
     
 </head>

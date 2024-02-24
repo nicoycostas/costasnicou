812f55fecce11cb25a0f054694a4f4b3c57a5c49
Costas Nicou <nicoycostas@gmail.com>
2024-02-24 03:28:41 +0200

    additional-reading

--- front-page.php
+++ front-page.php
@@ -86,6 +86,48 @@
         </div>
     </section>
 
+    <section class="additional-reading">
+        <h2 class="caligraphy-header">Additional Reading</h2>
+        <!-- Slideshow container -->
+        <div class="slideshow-container">
+
+            <!-- Full-width images with number and caption text -->
+            <div class="mySlides fade">
+           
+                   
+                <div class="text">Red Hat RHCSA (EX200) Cert Guide</div>
+            </div>
+
+            <div class="mySlides fade">
+             
+                
+                <div class="text">HTML and CSS: Design and Build Websites</div>
+            </div>
+
+            <div class="mySlides fade">
+           
+                <div class="text">Atomic Design by Brad Frost</div>
+            </div>
+
+            <!-- <div class="mySlides fade">
+                <div class="text">Leader Without Title</div>
+           </div> -->
+
+            <!-- Next and previous buttons -->
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            </div>
+            <br>
+
+            <!-- The dots/circles -->
+            <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+            <!-- <span class="dot" onclick="currentSlide(4)"></span> -->
+        </div>
+    </section>
+
     <!-- Portfolio Web Work -->
     <section id="portfolio" class="portfolio">
         <h2>Portfolio</h2>
